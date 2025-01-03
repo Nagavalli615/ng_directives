4f83d7a1df7c521e28c868c5c546ff08d09906e3
nagavallli <vallikatta226@gmail.com>
2025-01-03 16:01:07 +0530

    add changes

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -1,13 +1,32 @@
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgIf,NgTemplateOutlet,NgTemplateOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'directives';
+  user:string = "nagavalli"
+isenable:boolean = false;
+
+logincount = 0;
+checkfordata(){
+  this.logincount ++;
+  console.log(this.logincount);
+}
+users:Array<string> =['valli','siva','karthiik']
+
+userObj :Array<any> = [ 
+   {id:1,name:'valli',gmail:'vallikatta226@gmail.com'},
+   {id:2,name:'siva',gmail:'siva@gmail.com'},
+   {id:3,name:'karthik',gmail:'karthik@gmail.com'}
+]
+addnew(){
+  let newuser = {id:5,name:'nagavalli',gmail:'nagavalli@gmail.com'}
+  this.userObj.push(newuser)
+}
 }
