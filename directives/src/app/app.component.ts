1983a19908638539f4ab327eea9a7ef47af1527d
nagavallli <vallikatta226@gmail.com>
2025-01-04 21:00:57 +0530

    complete structural directives ngIf,ngFor,ngSwitch with new and old syntaxes

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgIf,NgTemplateOutlet,NgTemplateOutlet,NgFor],
+  imports: [RouterOutlet,NgIf,NgTemplateOutlet,NgTemplateOutlet,NgFor,NgSwitch,NgSwitchCase,NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,6 +13,9 @@ export class AppComponent {
   user:string = "nagavalli"
 isenable:boolean = false;
 
+constructor(){
+this.userObj.length
+}
 logincount = 0;
 checkfordata(){
   this.logincount ++;
@@ -29,8 +32,14 @@ addnew(){
   let newuser = {id:5,name:'nagavalli',gmail:'nagavalli@gmail.com'}
   this.userObj.push(newuser)
 }
-ondelete(user:any){
-  this.userObj.pop()
+// ondelete(user:object){
+//   let index = this.userObj.indexOf(user)
+//   //console.log(index)
+//   this.userObj.splice(index,1)
+// }
+ondelete(index:number){
+  this.userObj.splice(index,1)
 }
 
+userrole: string = ''
 }
