dc80801da9231b89054a975ba42696b3df049ce5
nagavallli <vallikatta226@gmail.com>
2025-01-06 13:48:34 +0530

    devide directive file wise

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
+import { NgClass, NgFor, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgIf,NgTemplateOutlet,NgTemplateOutlet,NgFor,NgSwitch,NgSwitchCase,NgSwitchDefault],
+  imports: [RouterOutlet,NgIf,NgTemplateOutlet,NgTemplateOutlet,NgFor,NgSwitch,NgSwitchCase,NgSwitchDefault,NgStyle,NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -42,4 +42,10 @@ ondelete(index:number){
 }
 
 userrole: string = ''
+
+
+//attribue binding-- style binding
+
+isstyle: boolean = false;
+
 }
